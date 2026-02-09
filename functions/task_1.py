d6399845f796fa08f43b8bfaa4d526800b8ee261
Laura Kuo <laurak10006@gmail.com>
2026-02-08 22:03:34 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -2,11 +2,12 @@ import os
 import csv
 import random
 
-def task_1(folder='tutorial', output1='random_integers_1.csv'):
+def task_1(folder='tutorial', output1='dataset1.csv'):
     os.makedirs(folder, exist_ok=True)
-    path_out1 = os.path.join(folder, output1)
-    with open(path_out1, mode='w', newline='') as f1:
+    path1 = os.path.join(folder, output1)
+    with open(path1, mode='w', newline='') as f1:
         writer = csv.writer(f1)
+        writer.writerow(['value'])
         for _ in range(20):
-            writer.writerow([random.randint(-1000, 1000)])
-    faasr_put_file(local_file='random_integers_1.csv', remote_folder=folder, remote_file='random_integers_1.csv')
+            writer.writerow([random.randint(0, 100)])
+    faasr_put_file(local_file='dataset1.csv', remote_folder=folder, remote_file='dataset1.csv')
