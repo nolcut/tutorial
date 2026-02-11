c752bc7b194cb5371135f1a65af618d45c69f114
Laura Kuo <laurak10006@gmail.com>
2026-02-11 12:55:04 -0800

    update functions/task_2.py

--- functions/task_2.py
+++ functions/task_2.py
@@ -1,15 +1,18 @@
 import os
-import csv
-import random
+import pandas as pd
+import numpy as np
 
-def task_2(folder='tutorial', output1='dataset2.csv'):
+def task_2(folder='tutorial', input1='dataset1.csv', input2='dataset2.csv', output1='summed_dataset.csv'):
     os.makedirs(folder, exist_ok=True)
-    data1 = [random.uniform(0, 100) for _ in range(100)]
-    data2 = [random.uniform(0, 100) for _ in range(100)]
-    output_path = os.path.join(folder, output1)
-    with open(output_path, mode='w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['value'])
-        for val in data2:
-            writer.writerow([val])
-    faasr_put_file(local_file='dataset2.csv', remote_folder=folder, remote_file='dataset2.csv')
+    length = 100
+    data1 = pd.DataFrame(np.random.randint(0, 100, size=length), columns=['value'])
+    data2 = pd.DataFrame(np.random.randint(0, 100, size=length), columns=['value'])
+    data1.to_csv(os.path.join(folder, input1), index=False)
+    data2.to_csv(os.path.join(folder, input2), index=False)
+    faasr_get_file(remote_folder=folder, remote_file='dataset1.csv', local_file='dataset1.csv')
+    faasr_get_file(remote_folder=folder, remote_file='dataset2.csv', local_file='dataset2.csv')
+    df1 = pd.read_csv(os.path.join(folder, input1))
+    df2 = pd.read_csv(os.path.join(folder, input2))
+    summed = df1['value'] + df2['value']
+    pd.DataFrame(summed, columns=['value']).to_csv(os.path.join(folder, output1), index=False)
+    faasr_put_file(local_file='summed_dataset.csv', remote_folder=folder, remote_file='summed_dataset.csv')
