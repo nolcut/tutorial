0add91e09dcd905e62f354feed671150f6bf51aa
Laura Kuo <laurak10006@gmail.com>
2026-02-15 14:53:00 -0800

    update functions/task_2.py

--- functions/task_2.py
+++ functions/task_2.py
@@ -1,34 +1,14 @@
 import os
 import csv
+import random
 
-def task_2(folder='tutorial', input1='dataset1.csv', input2='dataset2.csv', output1='combined_output.csv'):
-    faasr_get_file(remote_folder=folder, remote_file='dataset1.csv', local_file='dataset1.csv')
-    faasr_get_file(remote_folder=folder, remote_file='dataset2.csv', local_file='dataset2.csv')
+def task_2(folder='tutorial', output1='dataset2.csv'):
     os.makedirs(folder, exist_ok=True)
-    path1 = os.path.join(folder, input1)
-    path2 = os.path.join(folder, input2)
-    out_path = os.path.join(folder, output1)
-    data1 = []
-    data2 = []
-    with open(path1, newline='') as f1:
-        reader1 = csv.reader(f1)
-        for row in reader1:
-            if row:
-                try:
-                    data1.append(float(row[0]))
-                except ValueError:
-                    pass
-    with open(path2, newline='') as f2:
-        reader2 = csv.reader(f2)
-        for row in reader2:
-            if row:
-                try:
-                    data2.append(float(row[0]))
-                except ValueError:
-                    pass
-    combined = [(d1, d2, d1 + d2) for d1, d2 in zip(data1, data2)]
-    with open(out_path, 'w', newline='') as fout:
-        writer = csv.writer(fout)
-        writer.writerow(['Dataset1', 'Dataset2', 'Sum'])
-        writer.writerows(combined)
-    faasr_put_file(local_file='combined_output.csv', remote_folder=folder, remote_file='combined_output.csv')
+    dataset2 = [random.randint(1, 100) for _ in range(10)]
+    output_path = os.path.join(folder, output1)
+    with open(output_path, mode='w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['dataset2'])
+        for val in dataset2:
+            writer.writerow([val])
+    faasr_put_file(local_file='dataset2.csv', remote_folder=folder, remote_file='dataset2.csv')
