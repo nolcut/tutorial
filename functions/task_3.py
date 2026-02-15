16ddfaacd0346d052f924831f47089149a6501d2
Laura Kuo <laurak10006@gmail.com>
2026-02-15 14:53:02 -0800

    update functions/task_3.py

--- functions/task_3.py
+++ functions/task_3.py
@@ -1,22 +1,23 @@
 import os
-import json
+import csv
 
-def task_3(folder='tutorial', input1='dataset1.json', input2='dataset2.json', input3='summed_dataset.json', output1='final_output.json'):
-    faasr_get_file(remote_folder=folder, remote_file='dataset1.json', local_file='dataset1.json')
-    faasr_get_file(remote_folder=folder, remote_file='dataset2.json', local_file='dataset2.json')
-    faasr_get_file(remote_folder=folder, remote_file='summed_dataset.json', local_file='summed_dataset.json')
+def task_3(folder='tutorial', input1='dataset1.csv', input2='dataset2.csv', output1='combined_output.csv'):
+    faasr_get_file(remote_folder=folder, remote_file='dataset1.csv', local_file='dataset1.csv')
+    faasr_get_file(remote_folder=folder, remote_file='dataset2.csv', local_file='dataset2.csv')
     os.makedirs(folder, exist_ok=True)
     path1 = os.path.join(folder, input1)
     path2 = os.path.join(folder, input2)
-    path3 = os.path.join(folder, input3)
     out_path = os.path.join(folder, output1)
-    with open(path1, 'r') as f1, open(path2, 'r') as f2:
-        dataset1 = json.load(f1)
-        dataset2 = json.load(f2)
-    summed_dataset = [a + b for a, b in zip(dataset1, dataset2)]
-    with open(path3, 'w') as f3:
-        json.dump(summed_dataset, f3)
-    combined = {'dataset1': dataset1, 'dataset2': dataset2, 'summed_dataset': summed_dataset}
-    with open(out_path, 'w') as fout:
-        json.dump(combined, fout, indent=2)
-    faasr_put_file(local_file='final_output.json', remote_folder=folder, remote_file='final_output.json')
+    with open(path1, newline='') as f1, open(path2, newline='') as f2:
+        reader1 = csv.reader(f1)
+        reader2 = csv.reader(f2)
+        next(reader1, None)
+        next(reader2, None)
+        data1 = [float(row[0]) for row in reader1 if row]
+        data2 = [float(row[0]) for row in reader2 if row]
+    combined = [(d1, d2, d1 + d2) for d1, d2 in zip(data1, data2)]
+    with open(out_path, 'w', newline='') as fout:
+        writer = csv.writer(fout)
+        writer.writerow(['dataset1', 'dataset2', 'sum'])
+        writer.writerows(combined)
+    faasr_put_file(local_file='combined_output.csv', remote_folder=folder, remote_file='combined_output.csv')
