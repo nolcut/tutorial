588462689a7422b3b1ed16d621df3e90a3e5a99f
Laura Kuo <laurak10006@gmail.com>
2026-02-15 14:52:58 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -2,22 +2,13 @@ import os
 import csv
 import random
 
-def task_1(folder='tutorial', output1='dataset1.csv', output2='dataset2.csv'):
+def task_1(folder='tutorial', output1='dataset1.csv'):
     os.makedirs(folder, exist_ok=True)
-    length = 10
-    data1 = [round(random.uniform(1, 100), 2) for _ in range(length)]
-    data2 = [round(random.uniform(1, 100), 2) for _ in range(length)]
-    path1 = os.path.join(folder, output1)
-    path2 = os.path.join(folder, output2)
-    with open(path1, mode='w', newline='') as f1:
-        writer1 = csv.writer(f1)
-        writer1.writerow(['value'])
-        for val in data1:
-            writer1.writerow([val])
-    with open(path2, mode='w', newline='') as f2:
-        writer2 = csv.writer(f2)
-        writer2.writerow(['value'])
-        for val in data2:
-            writer2.writerow([val])
+    dataset1_path = os.path.join(folder, output1)
+    data = [random.uniform(0, 100) for _ in range(20)]
+    with open(dataset1_path, mode='w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['dataset1'])
+        for value in data:
+            writer.writerow([value])
     faasr_put_file(local_file='dataset1.csv', remote_folder=folder, remote_file='dataset1.csv')
-    faasr_put_file(local_file='dataset2.csv', remote_folder=folder, remote_file='dataset2.csv')
